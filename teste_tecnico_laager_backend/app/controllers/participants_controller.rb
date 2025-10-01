02be8b1e32d3b34a1d3b99e44464c8d433a236d5
Arthur Fernandes <arthur0139@gmail.com>
2025-09-30 22:30:16 -0300

    feat: add logger

--- teste_tecnico_laager_backend/app/controllers/participants_controller.rb
+++ teste_tecnico_laager_backend/app/controllers/participants_controller.rb
@@ -1,21 +1,29 @@
 class ParticipantsController < ApplicationController
   # Comentando rotas que não serão usadas mas que foram criadas pelo scaffold
-  before_action :set_participant, only: %i[ upvote ]
+  before_action :set_participant, only: %i[ upvote show ]
 
   # PATCH /participants/1/upvote
   def upvote
+    Rails.logger.info("Incrementing vote for participant #{@participant.id}")
     @participant.increment!(:total_votes)
+    rescue => err
+      Rails.logger.error(err)
   end
 
   # GET /participants
   def index
     @participants = Participant.all.order_by_name
+    rescue => err
+      Rails.logger.error(err)
     render json: @participants
   end
 
   # GET /participants/statistics
   def statistics
     @participants = Participant.select_only_name_and_total_votes
+    rescue => err
+      Rails.logger.error(err)
+
     total_votes = @participants.sum_total_votes
     render json: {
       total_votes: total_votes,
@@ -23,12 +31,12 @@ class ParticipantsController < ApplicationController
     }
   end
 
-  # Comentando rotas que não serão usadas mas que foram criadas pelo scaffold
-  # # GET /participants/1
-  # def show
-  #   render json: @participant
-  # end
+  # GET /participants/1
+  def show
+    render json: @participant
+  end
 
+  # Comentando rotas que não serão usadas mas que foram criadas pelo scaffold
   # # POST /participants
   # def create
   #   @participant = Participant.new(participant_params)
@@ -57,7 +65,22 @@ class ParticipantsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_participant
-      @participant = Participant.find(params[:id])
+      id = params[:id]
+
+      # unless é um if ao contrario
+      # nesse caso verificando se o id não é um número
+      unless id.to_s.match?(/\A\d+\z/)
+        Rails.logger.error("Id #{id} is not a number")
+        render status: :bad_request
+        return
+      end
+
+      # not found se o participante não for encontrado
+      @participant = Participant.find(id)
+      rescue => err
+        Rails.logger.error("Participant not found with id #{id} #{err}")
+        render status: :not_found
+        return
     end
 
     # Only allow a list of trusted parameters through.
--- testing.sh
+++ testing.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+# antes de testar necessario instalar sudo apt-get install apache2-utils
 # fazendo 1000 requisições paralelas simulando pessoas votando
 ab -n 10000 -c 1000 -m PATCH http://localhost:9999/api/participants/1/upvote &
 ab -n 10000 -c 1000 -m PATCH http://localhost:9999/api/participants/2/upvote
